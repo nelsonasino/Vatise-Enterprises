b885f7689b7c8af10e0566f995a041a93be4f060
asino-nelson <asinonelson276@gmail.com>
2023-07-09 21:19:50 +0300

    appartments update

--- src/components/AppartmentImage.jsx
+++ src/components/AppartmentImage.jsx
@@ -5,24 +5,21 @@ const cardVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const ImageCard = ({ source, type, bathrooms, squareArea }) => {
+const AppartmentImage = ({ source, bedrooms }) => {
   return (
     <div
       variants={cardVariant}
-      className="relative cursor-pointer w-full h-full row-span-1 md:col-span-1 col-span-2
+      className="relative cursor-pointer w-full h-full row-span-2 md:col-span-1 col-span-2 
       rounded-md overflow-hidden "
     >
       <div
         className="absolute h-full w-full p-10 opacity-0 hover:opacity-90  transform transition-all duration-500
          bg-black/70 z-30 inset-0 flex flex-col justify-center items-center text-center text-wheat"
       >
-        <p className="text-2xl">{type}</p>
-        <p className="mt-2">livingroom</p>
-        <p className="mt-2">dining room</p>
-        <p className="mt-2">bathrooms : {bathrooms} </p>
+        <p className="mt-2"> {bedrooms} bedroom(s) </p>
+        <p className="mt-2">living area</p>
+        <p className="mt-2">dining area</p>
         <p className="mt-2">kitchen</p>
-        <p className="mt-2">study</p>
-        <p className="mt-2">SqrArea : {squareArea}</p>
       </div>
 
       <img
@@ -34,4 +31,6 @@ const ImageCard = ({ source, type, bathrooms, squareArea }) => {
   );
 };
 
-export default ImageCard;
+export default AppartmentImage;
+
+
--- src/screens/Appartments.jsx
+++ src/screens/Appartments.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import ImageCard from "../components/ImageCard";
 import VideoCard from "../components/VideoCard";
+import AppartmentImage from "../components/AppartmentImage";
 
 const container = {
   hidden: {},
@@ -18,65 +19,23 @@ const Appartments = () => {
       </h1>
 
       <motion.div
-        className="w-full mb-8 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
+        className="w-full mb-10 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
         initial="hidden"
         whileInView="visible"
         variants={container}
       >
-        <ImageCard
+        <AppartmentImage
           source={"/images/3 bedroom hidden roof.png"}
-          type={"House"}
-          bathrooms={2}
-          squareArea={"100m"}
+          bedrooms={2}
         />
 
-        <VideoCard
-          source={"/images/LUMION RENDER GROUP - Facebook.mp4"}
-        />
-
-        <ImageCard
-          source={"/images/bungalow 3 bedroom - Picture1.png"}
-          type={"House"}
-          bathrooms={1}
-          squareArea={"100m"}
-        />
+        <VideoCard source={"/images/LUMION RENDER GROUP - Facebook.mp4"} />
 
         <ImageCard
           source={"/images/TELFORD 3 BEDROOM - Picture1.png"}
           type={"House"}
           bathrooms={1}
           squareArea={"100m"}
-        />
-      </motion.div>
-
-      <motion.div
-        className="w-full mb-8 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
-        initial="hidden"
-        whileInView="visible"
-        variants={container}
-      >
-        <ImageCard
-          source={"/images/3 bedroom hidden roof.png"}
-          type={"House"}
-          bathrooms={2}
-          squareArea={"100m"}
-        />
-
-        <ImageCard
-          source={"/images/bungalow 3 bedroom - Picture1.png"}
-          type={"House"}
-          bathrooms={1}
-          squareArea={"100m"}
-        />
-
-        <ImageCard
-          source={"/images/TELFORD 3 BEDROOM - Picture1.png"}
-          type={"House"}
-          bathrooms={1}
-          squareArea={"100m"}
-        />
-        <VideoCard
-          source={"/images/LUMION RENDER GROUP - Facebook.mp4"}
         />
         <ImageCard
           source={"/images/TELFORD 3 BEDROOM - Picture1.png"}
@@ -93,21 +52,64 @@ const Appartments = () => {
       </motion.div>
 
       <motion.div
-        className="w-full mb-8 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
+        className="w-full my-10 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
         initial="hidden"
         whileInView="visible"
         variants={container}
       >
-        <VideoCard
-          source={"/images/LUMION RENDER GROUP - Facebook.mp4"}
-        />
-
         <ImageCard
           source={"/images/3 bedroom hidden roof.png"}
           type={"House"}
           bathrooms={2}
           squareArea={"100m"}
         />
+
+        <ImageCard
+          source={"/images/bungalow 3 bedroom - Picture1.png"}
+          type={"House"}
+          bathrooms={1}
+          squareArea={"100m"}
+        />
+
+        <ImageCard
+          source={"/images/TELFORD 3 BEDROOM - Picture1.png"}
+          type={"House"}
+          bathrooms={1}
+          squareArea={"100m"}
+        />
+        <VideoCard source={"/images/LUMION RENDER GROUP - Facebook.mp4"} />
+        <AppartmentImage
+          source={"/images/3 bedroom hidden roof.png"}
+          bedrooms={2}
+        />
+      </motion.div>
+
+      <motion.div
+        className="w-full my-10 grid md:max-h-[100vh] md:grid-cols-3 grid-cols-2 md:grid-rows-3 grid-rows-4 gap-5"
+        initial="hidden"
+        whileInView="visible"
+        variants={container}
+      >
+        <ImageCard
+          source={"/images/3 bedroom hidden roof.png"}
+          type={"House"}
+          bathrooms={2}
+          squareArea={"100m"}
+        />
+
+        <ImageCard
+          source={"/images/bungalow 3 bedroom - Picture1.png"}
+          type={"House"}
+          bathrooms={1}
+          squareArea={"100m"}
+        />
+
+        <AppartmentImage
+          source={"/images/3 bedroom hidden roof.png"}
+          bedrooms={2}
+        />
+
+        <VideoCard source={"/images/LUMION RENDER GROUP - Facebook.mp4"} />
 
         <ImageCard
           source={"/images/bungalow 3 bedroom - Picture1.png"}
